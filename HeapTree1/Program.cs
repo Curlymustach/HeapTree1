ef07cfd89622096ab21697fc436d4312eab8cd4b
Meghana Sagare <meghanasagare@gmail.com>
2018-05-13 10:47:34 -0700

    Initial Commit

---
+++ HeapTree1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapTree1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> tree = new Tree<int>(6);
+            for (int i = 1; i <= 6; i++)
+            {
+                tree.Insert(i);
+            }
+            tree.Print();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HeapTree1/Tree.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeapTree1
+{
+    class Tree<T> where T : IComparable<T>
+    {
+        T[] tree;
+        int index = 0;
+
+        public Tree(int size)
+        {
+            tree = new T[size];
+        }
+
+        public int Left(int i)
+        {
+            return i * 2 + 1;
+        }
+
+        public int Right(int i)
+        {
+            return i * 2 + 2;
+        }
+
+        public int Parent(int i)
+        {
+            return (i - 1) / 2;
+        }
+
+        public void Insert(T value)
+        {
+            if (index == tree.Length)
+            {
+                Console.WriteLine("Heap Full.");
+            }
+            else
+            {
+                tree[index] = value;
+                HeapifyUp(index);
+                index++;
+            }
+        }
+
+        public void HeapifyUp(int i)
+        {
+            while (tree[i].CompareTo(tree[Parent(i)]) > 0)
+            {
+                T temp = tree[i];
+                tree[i] = tree[Parent(i)];
+                tree[Parent(i)] = temp;
+                //i = ?
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < tree.Length; i++)
+            {
+                Console.WriteLine(tree[i]);
+            }
+        }
+
+
+    }
+}
